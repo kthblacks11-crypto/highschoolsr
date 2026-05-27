a65119baa341840c178f6b6557eee54e9ecc2108
kthblacks11 <kthblacks11@gmail.com>
2026-05-27 03:47:33 +0000

    깃 암호화된 코드

---
+++ my-math-app/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
+
+export default defineConfig({
+  build: {
+    sourcemap: false, 
+    minify: 'esbuild', 
+    outDir: '../',
+  },
+  plugins: [
+    obfuscatorPlugin({
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        deadCodeInjection: true,
+        debugProtection: true,
+        stringArray: true,
+        stringArrayEncoding: ['base64'],
+      },
+    }),
+  ],
+});
